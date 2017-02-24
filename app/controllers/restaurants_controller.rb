9c9f1eb4b2ef51954ea02a6f5a7a1c7829036790
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2017-02-24 13:09:55 -0500

    validation and image uploader

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -69,6 +69,6 @@ class RestaurantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :number_of_seats, :cuisine)
+      params.require(:restaurant).permit(:name, :address, :number_of_seats, :cuisine, :image)
     end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates :name, :address, :number_of_seats, :cuisine, presence: true
+
+  mount_uploader :image, ImageUploader
 end
